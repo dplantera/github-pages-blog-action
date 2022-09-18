05428790da0c6e44778459eb6db8bd916bf588ae
danie <daniele.plantera@outlook.de>
2022-09-18 13:07:01 +0200

    darkmode hrhr release v0.1.9

--- generate.ts
+++ generate.ts
@@ -1,14 +1,13 @@
-import * as path from 'path';
-import { prepareTheme } from './src/theme';
-import { SiteConfigType } from './src/theme/types';
+import * as path from "path";
+import { prepareTheme } from "./src/theme";
+import { SiteConfigType } from "./src/theme/types";
 
-const repoPath = path.join(process.cwd(), '../dplantera.github.io-tech-blog/');
-const outputDir = path.join(process.cwd(), './out');
+const repoPath = path.join(process.cwd(), "../dplantera.github.io-tech-blog/");
+const outputDir = path.join(process.cwd(), "./out");
 
-const siteConfig: SiteConfigType = require(path.join(repoPath, 'site.json'));
-siteConfig.baseUrl = '';
-siteConfig.repoName = '.';
+const siteConfig: SiteConfigType = require(path.join(repoPath, "site.json"));
+siteConfig.baseUrl = "";
 
-prepareTheme({ repoPath, outputDir, siteConfig, themeDir: path.join(process.cwd(), 'theme') })
-  .then(() => console.log('generated site'))
+prepareTheme({ repoName: ".", repoPath, outputDir, siteConfig, themeDir: path.join(process.cwd(), "theme") })
+  .then(() => console.log("generated site"))
   .catch(error => console.error(error));
